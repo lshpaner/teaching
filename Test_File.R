a5dcd13175a07cbec5123e6e49427f628f579684
lshpaner <lshpaner@ucla.edu>
2022-01-13 17:34:02 -0800

    added new .R file

---
+++ Test_File.R
@@ -0,0 +1,25 @@
+# To install a package or library, simply type in `install.packages('package_name').` 
+# For the following exercises, let us ensure that we have installed the following packages:
+install.packages('psych') # psychological library as extension for statistical tools
+install.packages('readr') # for reading rectangular data (i.e., 'csv', 'tsv', and 'fwf')
+install.packages('summarytools')  # additional library for summarizing data
+install.packages('caret') # classification and regression training (modeling)
+install.packages('e1071') # other miscellaneous function
+install.packages('rpart') # for classification and decision trees
+install.packages('rpart.plot') # for plotting classification and decision trees
+install.packages('cluster') # methods for cluster analysis
+install.packages('factoextra') # clustering algorithms & visualization
+# To read the documentation for any given library, simply put a "?" before the library name
+# and press "enter."
+
+
+# To read in the libraries
+library(psych)
+library(readr)
+library(summarytools)
+library(caret)
+library(e1071)
+library(rpart)
+library(rpart.plot)
+library(cluster)
+library(factoextra)
